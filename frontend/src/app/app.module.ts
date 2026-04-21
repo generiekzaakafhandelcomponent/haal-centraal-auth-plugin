fa0d53ec91dc24dc868bf13464b290f3c7e6cb0a
Klaas Schuijtemaker <klaas.schuijtemaker@ritense.com>
2026-04-21 12:30:21 +0200

    Fix app.module.ts: use HaalCentraalBrpAuthPluginModule and haalCentraalBrpAuthPluginSpecification

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -98,7 +98,7 @@ import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
 import {environment} from "../environments/environment";
 import {registerDocumentenApiFormioUploadComponent, ZgwModule} from "@valtimo/zgw";
 
-import {SamplePluginModule, samplePluginSpecification,} from "@valtimo-plugins/haal-centraal-auth";
+import {HaalCentraalBrpAuthPluginModule, haalCentraalBrpAuthPluginSpecification,} from "@valtimo-plugins/haal-centraal-auth";
 
 export function tabsFactory() {
   return new Map<string, object>([
@@ -158,7 +158,7 @@ export function tabsFactory() {
     ProcessModule,
     ReactiveFormsModule,
     ResourceModule,
-    SamplePluginModule,
+    HaalCentraalBrpAuthPluginModule,
     SecurityModule,
     SseModule,
     SwaggerModule,
@@ -190,7 +190,7 @@ export function tabsFactory() {
         objecttypenApiPluginSpecification,
         openNotificatiesPluginSpecification,
         openZaakPluginSpecification,
-        samplePluginSpecification,
+        haalCentraalBrpAuthPluginSpecification,
         zakenApiPluginSpecification,
       ],
     },
